e18bf9707d2f2be27ab8774498c1f5bb09932ee2
Thais B <thais_brt@hotmail.com>
2023-02-25 18:12:19 -0300

    feat: criando model artigo e salvando teste no banco de dados

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose');
+const articleModel = require('./models/article');
 
 mongoose.connect('mongodb://localhost:27017/aprendendoMongo');
+
+const Article = mongoose.model('Article', articleModel);
+
+const article = new Article({
+    title: 'Teste título',
+    author: 'Teste autor',
+    body: 'Testando o corpo do artigo'
+});
+
+article.save().then(() => {
+    console.log('Artigo salvo!');
+}).catch((err) => {
+    console.log(err);
+});
